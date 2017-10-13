c13d9fedddc3c666d7eada30318e26bbb6c7f6d3
shravani133 <shravani133@siu.edu>
2017-10-12 22:47:49 -0400

    Initial commit

---
+++ src/HelloSanku.java
@@ -0,0 +1,6 @@
+public class HelloSanku {
+    //This is a sample program for Sanku
+    public static void main(String[] args) {
+        System.out.println("Hello!! Sankuuu.. I made it");
+    }
+}
---
+++ src/com/example/hellosanku/HelloSanku.java
@@ -0,0 +1,7 @@
+package com.example.hellosanku;
+
+public class HelloSanku {
+    public static void main(String[] args) {
+        System.out.println("hello Sanku! I made it");
+    }
+}
